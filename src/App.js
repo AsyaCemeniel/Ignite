f7ba9b3343034fdc01c6aaa7a6ecac6bce160875
AsyaCemeniel <asyaorlov89@gmail.com>
2021-03-16 15:09:16 +0200

    Add Global Styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 //Components and Pages
 import Home from "./pages/Home";
+//Style
+import GlobalStyles from "./components/GlobalStyles";
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyles />
       <Home />
     </div>
   );
